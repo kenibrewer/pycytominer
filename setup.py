090f2a7c042089926b4842a352b1e61ef0c7d614
Ken Brewer <kenibrewer@users.noreply.github.com>
2023-04-22 15:10:44 +0000

    Fix merge conflicts

--- setup.py
+++ setup.py
@@ -28,6 +28,12 @@ setup(
     install_requires=REQUIRED_PKGS,
     extras_require={
         "collate": ["cytominer-database==0.3.4"],
+        "cell_locations": [
+            "fsspec>=2023.1.0",
+            "s3fs>=0.4.2",
+            "boto3>=1.26.79",
+            "fire>=0.5.0",
+        ],
         "dev": DEV_PKGS,
     },
     python_requires=">=3.4",
